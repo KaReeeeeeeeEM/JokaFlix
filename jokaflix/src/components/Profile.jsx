74ccd197d553ea94b08668933196e88047cbcd58
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-06-16 18:02:48 +0300

    placed a cover page in the home section

---
+++ jokaflix/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+        <img src='https://avatars.githubusercontent.com/u/70122135?v=4' alt='profile' className='border-2 border-orange-400 rounded-full w-8 h-8 md:w-10 md:h-10'/>
+    </div>
+  )
+}
+
+export default Profile
