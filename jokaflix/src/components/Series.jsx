9178a396fc99f2626c95de869532a3765072bc1d
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-06-17 21:33:50 +0300

    finished the homepage

--- jokaflix/src/components/Series.jsx
+++ jokaflix/src/components/Series.jsx
@@ -78,7 +78,6 @@ const Series = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="orange" className="size-6 mr-2">
                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z" />
             </svg>
-
             </span>
             Popular Series</h1>
         <button className='text-orange-300 text-md' onClick={() => setOpenModal(true)}>
