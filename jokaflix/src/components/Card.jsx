8217816ff57608e1d33afe9629ec4c9aa8841802
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-06-29 13:15:02 +0300

    made some changes

--- jokaflix/src/components/Card.jsx
+++ jokaflix/src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import imdb from '../assets/imdb.png';
+import hdIcon from '../assets/hdIcon.png';
+import video from '../assets/video.png'; 
 import star from '../assets/star.png';
 import CardLoader from './CardLoader';
 import '../card.css';
 
-const Card = ({ src, rating, loading, category }) => {
+const Card = ({ src, rating, loading, category, quality }) => {
   return (
     <div>
       {loading ? (
@@ -20,16 +22,36 @@ const Card = ({ src, rating, loading, category }) => {
           <div className="relative top-0 h-full w-full inset-0 bg-opacity-60 bg-gray-900 blur-md"></div>
           <div className='overlay'>
             {rating && (
-              <h1 className='overlay-rating flex flex-col items-center text-lg md:text-2xl'><img src={star} alt="star" className='star-img w-12 h-12' />{rating < 1 ? '5.2 / 10' : Math.ceil(rating * 10) / 10} / 10</h1>
+              <h1 className='overlay-rating flex flex-col items-center text-lg md:text-2xl'>
+                <img src={star} alt="star" className='star-img w-12 h-12' />
+                {rating < 1 ? '5.2 / 10' : Math.ceil(rating * 10) / 10} / 10
+              </h1>
             )}
             <div className='card-category'>
               <h1 className='category-text text-orange-400 text-[1.3rem] font-bold'>{category}</h1>
             </div>
           </div>
           <div className='content'>
-            <div className='flex items-center justify-start relative -top-60 md:-top-72 lg:-top-96 my-1 mx-4 w-[6rem] h-[2rem]'>
-              <img src={imdb} alt='imdb' className='w-[2rem] h-[2rem] mr-1' />
-              {rating && <h1 className='flex items-center text-lg text-white font-bold'><span className='w-6 h-4'><img src={star} alt="star" className='w-4 h-4 ml-1' /></span>{rating < 1 ? 5.2 : Math.ceil(rating * 10)/10}</h1>}
+            <div className='flex items-center justify-start relative -top-60 md:-top-72 lg:-top-96 my-1 mx-4 w-full h-[2rem]'>
+              <div className='w-1/2 flex'>
+                <img src={imdb} alt='imdb' className='w-[2rem] h-[2rem] mr-1' />
+                {rating && (
+                  <h1 className='flex items-center md:text-lg text-white font-bold'>
+                    <span className='w-6 h-4'>
+                      <img src={star} alt="star" className='w-4 h-4 ml-1' />
+                    </span>
+                    {rating < 1 ? 5.2 : Math.ceil(rating * 10) / 10}
+                  </h1>
+                )}
+              </div>
+              <div className='w-1/2 flex justify-end pr-6 md:pr-8'>
+                {quality && (
+                  quality === 'hd' ? (
+                    <img src={hdIcon} alt='HD' className='w-[1rem] h-[1rem] md:w-[1.5rem] md:h-[1.5rem] mr-1' />
+                  ) : (
+                    <img src={video} alt='Recorded' className='w-[1rem] h-[1rem] md:w-[1.5rem] md:h-[1.5rem] mr-1' />
+                  ))}
+              </div>
             </div>
             <div>
               <h1 className='relative bottom-28 md:bottom-24 uppercase text-center text-lg md:text-xl text-white font-bold'>{category}</h1>
