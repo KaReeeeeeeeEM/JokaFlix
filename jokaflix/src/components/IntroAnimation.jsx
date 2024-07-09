177dbe566d70335956ecaec426d53733bd5969c0
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-07-09 11:05:48 +0300

    made the JokaFlix intro text smaller for smaller devices

--- jokaflix/src/components/IntroAnimation.jsx
+++ jokaflix/src/components/IntroAnimation.jsx
@@ -4,7 +4,7 @@ import '../animation.css';
 const IntroAnimation = () => {
   return (
     <div className="intro-animation-container">
-      <div className="logo text-white">Joka<span className="text-orange-600">Flix</span></div>
+      <div className="logo text-white text-md md:text-2xl">Joka<span className="text-orange-600">Flix</span></div>
     </div>
   );
 };
