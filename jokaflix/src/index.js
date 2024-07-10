3da9a7ba92108f7978b4d3307e979e09e33b8a73
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-07-10 09:31:27 +0300

    tried to make JokaFlix a PWA by adding the manifest.json and service-workers

--- jokaflix/src/index.js
+++ jokaflix/src/index.js
@@ -5,6 +5,20 @@ import './scrollbar.css';
 import App from './App';
 
 
+// Import the Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('../service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
