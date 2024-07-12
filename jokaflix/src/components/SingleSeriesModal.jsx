55286906d31aa4e92e8ccc3f1288c8b6173c206f
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-07-12 12:57:56 +0300

    removed some stuff in the dependancy array in useEffect

--- jokaflix/src/components/SingleSeriesModal.jsx
+++ jokaflix/src/components/SingleSeriesModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useRef, useEffect, useState } from 'react';
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
