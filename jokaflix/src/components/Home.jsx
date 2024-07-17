c107934eda08a575ba43382f41227ebd8b707ad6
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-07-17 03:56:01 +0300

    moved the imdb image in the home page a bit on top

--- jokaflix/src/components/Home.jsx
+++ jokaflix/src/components/Home.jsx
@@ -263,7 +263,7 @@ const Home = () => {
               </button>
             </div>
           </div>
-          <div className='flex items-center justify-center h-16 md:h-24 my-12 w-[20vw] md:w-[8vw] m-auto'>
+          <div className='flex items-center justify-center h-16 md:h-24 my-2 md:my-12 w-[20vw] md:w-[8vw] m-auto'>
             <img src={imdb} alt="imdb-icon" className='w-full h-full' />
           </div>
           <div className='my-last-step'>
