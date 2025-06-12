74e400f5fa431d3db352d4affed6d31e1608e6c4
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2025-06-12 13:26:49 +0300

    Set the theme to permanently be dark

--- jokaflix/src/main.tsx
+++ jokaflix/src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
         attribute="class"
-        defaultTheme="system"
+        defaultTheme="dark"
         enableSystem
         disableTransitionOnChange
       >
