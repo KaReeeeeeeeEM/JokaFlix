4c446fa0d80bda4f00c142822b5ef20dd154fe3c
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2026-01-15 17:03:36 +0300

    feat: pass dynamic API URL to BottyWidget for improved functionality

--- jokaflix/src/pages/Home.tsx
+++ jokaflix/src/pages/Home.tsx
@@ -4,12 +4,15 @@ import TrendingMovies  from '../components/global/trending-movies/TrendingMovies
 import { BottyWidget } from 'botty-widget';
 
 export default function Home() {
+  const baseUrl = typeof window !== 'undefined' ? window.location.origin : 'https://jokaflix.vercel.app'
+  const bottyApiUrl = `${baseUrl}/api`
+
   return (
     <div className='relative w-full h-screen overflow-hidden'>
       <Hero />
       <TrendingMovies />
       <PopularMovies />
-      <BottyWidget botId='6968e1a8629bc8c27e18a5bf' />
+      <BottyWidget botId='6968e1a8629bc8c27e18a5bf' apiUrl={bottyApiUrl} />
     </div>
   )
 }
