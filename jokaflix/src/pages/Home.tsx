7d2c6d6d68bbb75a07800c816d1c065a289e29e0
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2026-01-15 15:51:20 +0300

    feat: Added a chatbot feature from Botty

--- jokaflix/src/pages/Home.tsx
+++ jokaflix/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Hero } from '../components/global/hero'
 import PopularMovies from '../components/global/popular-movies/PopularMovies'
 import TrendingMovies  from '../components/global/trending-movies/TrendingMovies'
+import { BottyWidget } from 'botty-widget';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <TrendingMovies />
       <PopularMovies />
+      <BottyWidget botId='6968e1a8629bc8c27e18a5bf' />
     </div>
   )
 }
